8d3b398ef9aba5f2c7ef2d12fca2465d515c7411
Chirag Wadyalkar <cwadyalkar@gmail.com>
2024-02-16 10:03:47 +0530

    unused file delete

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Routes, Route ,Navigate} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from '../Pages/Home';
 import Contact from '../Pages/Contact';
 import AboutUs from '../Pages/AboutUs';
-import Menu from '../Pages/Menu';
+
 
 
 const Router = () => {
   return (
    <Routes>
-    <Route path='/' element={<Navigate to="/home"/>}/>
-    <Route path='/home' element={<Home/>}/>
+    
+    <Route path='/' element={<Home/>}/>
     <Route path='/contact' element={<Contact/>}/>
     <Route path='/about' element={<AboutUs/>}/>
-    <Route path='/menu' element={<Menu/>}/>
+    
     
    </Routes>
   )
