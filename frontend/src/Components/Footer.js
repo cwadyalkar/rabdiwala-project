9a33541b97fd2355825835a2dcaedbb6e2b5fcab
Chirag Wadyalkar <cwadyalkar@gmail.com>
2024-02-16 10:26:18 +0530

    final commit

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -67,7 +67,7 @@ const Footer = () => {
       <Row className="mt-5">
         <Col lg="6" md="6">
           <p className="copyright__text">
-            Copyright - 2022, website made by Muhibur Rahman. All Rights
+            Copyright - 2024, website made by Idevify Solution. All Rights
             Reserved.
           </p>
         </Col>
